9e6b8a0395e128ce523be1f543bba9e6dbe465c2
ermishechkin <erman15@yandex.ru>
2017-04-15 15:38:58 +0300

    Add data serialization and RecyclerView

---
+++ app/src/main/java/com/bog/password_manager_android/PasswordModel.java
@@ -0,0 +1,13 @@
+package com.bog.password_manager_android;
+
+import java.util.Map;
+
+/**
+ * Created by alex on 14.04.2017.
+ */
+
+class PasswordModel {
+    String name;
+    String password;
+    Map<String, String> additionalFields;
+}
--- app/src/main/java/com/bog/password_manager_android/ResourceActivity.java
+++ app/src/main/java/com/bog/password_manager_android/ResourceActivity.java
@@ -2,12 +2,43 @@ package com.bog.password_manager_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.List;
 
 public class ResourceActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resource);
+
+        List<PasswordModel> res = PasswordsStringConvertor.deserialize(
+                "[{\"name\": \"Yandex.ru\", \"password\": \"keks\", \"additionalFields\": {\"login\": \"abc\"}}," +
+                        "{\"name\": \"Rambler.ru\", \"password\": \"lol\", \"additionalFields\": {\"login\": \"ee\"}}]");
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.passwordsList);
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new RecyclerViewAdapter(res);
+        mRecyclerView.setAdapter(mAdapter);
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(
+                mRecyclerView.getContext(), DividerItemDecoration.VERTICAL);
+        mRecyclerView.addItemDecoration(dividerItemDecoration);
     }
+
+
+
+
+
+
 }
